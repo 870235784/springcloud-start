71222a3088c226b99eca7513f9fb7bbb4484e6c9
zhouan <870235784@qq.com>
2019-05-21 20:07:13 +0800

    增加zuul网关过滤器

--- readme.md
+++ readme.md
@@ -257,8 +257,21 @@
   					consumer:
   						serviceId: client-consumer
   						path: /zuul-consumer/**
-		
-
+	8.3 网关过滤器ZuulFilter
+		8.3.1 Filter是Zuul的核心，用来实现对外服务的控制
+		8.3.2 类型:
+			PRE： 这种过滤器在请求被路由之前调用。我们可利用这种过滤器实现身份验证、在集群中选择请求的微服务、记录调试信息等。
+			ROUTING：这种过滤器将请求路由到微服务。这种过滤器用于构建发送给微服务的请求，并使用Apache HttpClient或Netfilx Ribbon请求微服务。
+			POST：这种过滤器在路由到微服务以后执行。这种过滤器可用来为响应添加标准的HTTP Header、收集统计信息和指标、将响应从微服务发送给客户端等。
+			ERROR：在其他阶段发生错误时执行该过滤器。 除了默认的过滤器类型，Zuul还允许我们创建自定义的过滤器类型。例如，我们可以定制
+					一种STATIC类型的过滤器，直接在Zuul中生成响应，而不将请求转发到后端的微服务。
+		8.3.3 自定义实现Filter
+			第一步:自定义Filter类继承,使用@Component注册
+			第二步:重写四个方法
+				String filterType(); //定义filter的类型，有pre、route、post、error四种
+				int filterOrder(); //定义filter的顺序，数字越小表示顺序越高，越先执行
+				boolean shouldFilter(); //表示是否需要执行该filter，true表示执行，false表示不执行
+				Object run(); //filter需要执行的具体操作
 ================================================================================================
 
 9.注册中心Eureka高可用配置
---
+++ springcloud-start-zuul/src/main/java/com/tca/filter/TokenFilter.java
@@ -0,0 +1,53 @@
+package com.tca.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class TokenFilter extends ZuulFilter{
+	
+	private static final Logger logger = LoggerFactory.getLogger(TokenFilter.class);
+	
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext requestContext = RequestContext.getCurrentContext();
+		HttpServletRequest request = requestContext.getRequest();
+		String token = (String)request.getParameter("token");
+		logger.info("请求token为:{}, {}", token, request.getRequestURI());
+		if ("tca".equals(token)) {
+			logger.info("token验证通过");
+			requestContext.setSendZuulResponse(true); //对请求进行路由
+			requestContext.setResponseStatusCode(200);
+			requestContext.set("isSuccess", true);
+            return null;
+		}
+		logger.info("token验证失败");
+		requestContext.setSendZuulResponse(false); //不对其进行路由
+		requestContext.setResponseStatusCode(400);
+		requestContext.set("isSuccess", false);
+		return "token失效";
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+}
